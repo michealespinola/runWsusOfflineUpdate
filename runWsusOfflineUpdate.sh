9b57fc947eb088911837d329a22f7321e12e0aaa
Micheal Espinola <michealespinola@users.noreply.github.com>
2022-08-30 12:23:33 -0700

    Minor changes
    1. Fixed grep output comparison
    2. Changed sleep interval
    3. Added newlines to print statements

--- runWsusOfflineUpdate.sh
+++ runWsusOfflineUpdate.sh
@@ -13,19 +13,20 @@ docker start wsusoffline
 echo 
 
 # CHECK IF WSUSOFFLINE CONTAINER IS RUNNING
-if [ "$(docker ps  | grep "wsusoffline")" ]; then
+if docker ps | grep -q "wsusoffline"; then
   # WHILE RUNNING KEEP MONITORING FOR THE FINAL SCRIPT SLEEP COMMAND OF "SLEEP 48H"
   while [[ $(docker top wsusoffline | grep -o "sleep 48h") != "sleep 48h" ]]; do
-    printf '\r\e[K%s' "Waiting 15s to finish.."
-    sleep 15
+    printf '\r\e[K%s' "Waiting to finish (5s loop).."
+    sleep 5
     printf '\r\e[K'
   done
-  printf '\r\e[K%s' "Finished.."
+  printf '\r\e[K%s\n' "Finished.."
   # STOP THE WSUSOFFLINE CONTAINER WHEN THE SCRIPT IS FINISHED AND IS SLEEPING
+  #docker kill --signal=SIGINT wsusoffline
   docker stop -t 900 wsusoffline
 else
   # IF CONTAINER NOT RUNNING PRINT THIS MESSAGE
-  printf '%s' "wsusoffline container is not running.."
+  printf '%s\n' "wsusoffline container is not running.."
 fi
 
 # UNCOMMENT TO FORCE KILL IF YOU REALLY THINK ITS NECCESSARY
